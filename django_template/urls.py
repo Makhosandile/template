cb52bb9028baf6a428fdf8304c4b03467a451719
Makhosandile <mpnyamela@gmail.com>
2020-04-28 23:36:44 +0200

    Added custom user model.

--- django_template/urls.py
+++ django_template/urls.py
@@ -1,4 +1,4 @@
-"""template URL Configuration
+"""django_template URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
